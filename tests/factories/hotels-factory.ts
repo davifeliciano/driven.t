fd541ef7aa05cbb94876671182428b11e927a41c
Davi Feliciano <dfeliciano37@gmail.com>
2023-07-24 15:27:48 -0300

    test: bookings related tests

--- tests/factories/hotels-factory.ts
+++ tests/factories/hotels-factory.ts
@@ -1,16 +1,12 @@
 import faker from '@faker-js/faker';
 import { prisma } from '@/config';
 
-export async function createBooking(userId: number, roomId: number) {
-  return prisma.booking.create({ data: { userId, roomId } });
-}
-
-export async function createHotel() {
+export async function createHotel({ roomCapacity = faker.datatype.number() } = {}) {
   return prisma.hotel.create({
     data: {
       name: faker.company.companyName(),
       image: faker.image.imageUrl(),
-      Rooms: { create: { name: faker.word.noun(), capacity: faker.datatype.number() } },
+      Rooms: { create: { name: faker.word.noun(), capacity: roomCapacity } },
     },
     include: { Rooms: true },
   });
